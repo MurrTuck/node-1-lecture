9b6dc19e4b59e8119aa8930b85277233189d0e80
MurrTuck <murrtuck@gmail.com>
2020-05-18 13:29:57 -0600

    Lecture Complete

---
+++ nodeTest.js
@@ -0,0 +1,15 @@
+const andrew = {
+    name: 'Murray',
+    age: 34,
+    favorites: {
+        color: 'red',
+        number: 44,
+        person: 'Thor'
+    },
+}
+
+
+console.log(`Hello, my name is ${andrew.name}. I'm ${andrew.age} years old and I want to be ${andrew.favorites.person} when I grow up.`)
+
+
+
