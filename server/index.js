b9a6fc22c317e36fffd3505d1d2bb2d2de3e62d8
MurrTuck <murrtuck@gmail.com>
2020-05-19 14:09:30 -0600

    Lecture Complete

--- server/index.js
+++ server/index.js
@@ -5,13 +5,21 @@ const SERVER_PORT = 4000
 
 app.use(express.json())
 
+app.use(function (req, res, next) {
+    console.log("We got a request cap'n")
+    next()
+})
+
 app.get('/api/andrew', (req, res) => {
-    console.log('I GOT A REQUEST A /api/andrew')
+    // console.log('I GOT A REQUEST A /api/andrew')
     res.status(200).send('Andrew')
 })
 
 app.get('/api/users', ctrl.getAllUsers)
 app.get('/api/users/:user_id', ctrl.getUserById)
+app.post('/api/users', ctrl.createUser)
+app.put('/api/users/:user_id', ctrl.editUser)
+app.delete('/api/users/:user_id', ctrl.deleteUser)
 
 app.listen(SERVER_PORT, () =>
     console.log(`Server is running on port ${SERVER_PORT} (0,0)`)
